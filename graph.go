baa779604565b8e59d34fd222d7b559780a1b95a
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2017-01-08 18:52:33 +1100

    Move node scoring function into graph.go

--- graph.go
+++ graph.go
@@ -5,6 +5,9 @@ import "sort"
 // nodeInitialScore is the initial score of the node.
 const nodeInitialScore = 1
 
+// dampeningFactor is used when scoring text nodes, and is in the range (0, 1].
+const dampeningFactor = 0.85
+
 // textgraph is a textrank graph.
 type textgraph []*node
 
@@ -46,6 +49,19 @@ func (tg *textgraph) getNode(text string) *node {
 		}
 	}
 	return nil
+}
+
+// scoreNode calculates the voting score for a given node.
+func scoreNode(n *node, iterations int) float64 {
+	if iterations == 0 {
+		return 0
+	}
+
+	var successiveScore float64
+	for _, edges := range n.Edges {
+		successiveScore += scoreNode(edges, iterations-1)
+	}
+	return (1 - dampeningFactor) + dampeningFactor*successiveScore
 }
 
 // normalize normalizes the graph into a list containing the string of each
